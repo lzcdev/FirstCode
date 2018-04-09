ec5d6f95afad43af378de87dfca2f20ef99ed9fe
lzcdev <lzc2134@gmail.com>
2018-04-09 14:49:54 +0800

    聊天界面

---
+++ UIBestPractice/app/src/main/java/com/lzc/uibestpractice/Msg.java
@@ -0,0 +1,24 @@
+package com.lzc.uibestpractice;
+
+/**
+ * Created by lzc on 2018/4/9.
+ */
+
+public class Msg {
+
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SENT = 1;
+    private String content;
+    private int type;
+
+    public Msg(String content, int type) {
+        this.content = content;
+        this.type = type;
+    }
+    public String getContent() {
+        return content;
+    }
+    public int getType() {
+        return type;
+    }
+}
