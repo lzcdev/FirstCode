11034565aebfe32d0b0c5cad92b6df701dc09535
lzcdev <lzc2134@gmail.com>
2018-04-10 17:41:28 +0800

    新闻界面

---
+++ FragmentBestPractice/app/src/main/java/com/lzc/fragmentbestpractice/News.java
@@ -0,0 +1,24 @@
+package com.lzc.fragmentbestpractice;
+
+/**
+ * Created by lzc on 2018/4/10.
+ */
+
+public class News {
+
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
